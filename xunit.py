214d1f48e33b648c6151c2ed0641153404bccfde
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-12-03 18:27:10 +0900

    feat(ch19): call setup at first

--- xunit.py
+++ xunit.py
@@ -2,26 +2,36 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
+    def setUp(self):
         self.wasRun = None
-        super().__init__(name)
+        self.wasSetUp = True
 
     def testMethod(self):
         self.wasRun = True
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
